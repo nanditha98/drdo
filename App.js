79a66086b5b3120c7a3ffbf6a752d5cfced14df8
nanditha98 <102796257+nanditha98@users.noreply.github.com>
2022-12-08 07:13:14 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home';
-import Login from './components/Login';
-import Details from './components/Details';
-import Errror from './components/Error';
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
+import Header from './components/Header';
+import {AuthProvider} from './components/context/AuthContext';
 function App() {
   return (
-  <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/details' element={<Details />} />
-      <Route path='*' element={<Errror />} />
+ <div className='App'>
+  <BrowserRouter>
+  <AuthProvider>
+  <Header/>
+  <Routes>
+    
+    <Route  path='/' element={<HomePage/>} /> 
+    <Route  path='/login' element={<LoginPage/>} />
     </Routes>
-    </BrowserRouter>
-  </>
+    </AuthProvider>
+  </BrowserRouter>
+  
+ </div>
   );
 }
 
